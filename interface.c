c7fa14b228dc2e45dcec4d9192d18117c13e3eef
Bryton Lacquement <bryton.lacquement@nexedi.com>
2018-06-21 10:28:15 +0200

    Add /fibonacci handler

--- interface.c
+++ interface.c
@@ -1,16 +1,23 @@
 #include <lwan/lwan.h>
 
 extern int handle_root(struct lwan_request *request, struct lwan_response *response);
+extern int handle_fibonacci(struct lwan_request *request, struct lwan_response *response);
 
 LWAN_HANDLER(root)
 {
   return handle_root(request, response);
 }
 
+LWAN_HANDLER(fibonacci)
+{
+  return handle_fibonacci(request, response);
+}
+
 static int listen_and_serve(void)
 {
   const struct lwan_url_map default_map[] = {
     { .prefix = "/", .handler = LWAN_HANDLER_REF(root) },
+    { .prefix = "/fibonacci", .handler = LWAN_HANDLER_REF(fibonacci) },
     { .prefix = NULL }
   };
   struct lwan l;
--- lwan_wrapper.pyx
+++ lwan_wrapper.pyx
@@ -19,6 +19,7 @@ cdef extern from "lwan/lwan-strbuf.h":
     pass
 
   bint lwan_strbuf_set_static(lwan_strbuf *s1, const char *s2, size_t sz) nogil
+  bint lwan_strbuf_printf(lwan_strbuf *s, const char *fmt, ...) nogil
 
 cdef extern from "interface.c":
   int listen_and_serve() nogil
@@ -32,5 +33,21 @@ cdef public int handle_root(lwan_request *request, lwan_response *response) nogi
   response.mime_type = "text/plain"
   
   lwan_strbuf_set_static(response.buffer, message, strlen(message))
+  
+  return HTTP_OK
+
+cdef unsigned int fibonacci(unsigned int n) nogil:
+  cdef unsigned int i, a, b
+  a, b = 0, 1
+  
+  for i in range(n):
+    a, b = b, a + b
+  
+  return a
+
+cdef public int handle_fibonacci(lwan_request *request, lwan_response *response) nogil:
+  response.mime_type = "text/plain"
+  
+  lwan_strbuf_printf(response.buffer, "Fibonacci(10^6) = %u (with overflow)\n", fibonacci(1000000))
   
   return HTTP_OK
